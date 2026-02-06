6dbf10f7e11d1618efe2660796e64ef4dd39588f
khalshaqzzy <19623094@std.stei.itb.ac.id>
2026-02-06 22:15:27 +0700

    removed compilation files

--- backend/src/services/externalDataService.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class ExternalDataService {
-    /**
-     * Fetches the nearest mineral deposit data from Kaggle dataset in BigQuery
-     */
-    static getNearestDeposits(lat: number, lon: number, limit?: number): Promise<any[]>;
-}
-//# sourceMappingURL=externalDataService.d.ts.map
--- backend/src/services/inferenceService.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class InferenceService {
-    /**
-     * Orchestrates the hybrid inference process
-     */
-    static predictGrade(projectName: string, polygon: [number, number][]): Promise<any>;
-}
-//# sourceMappingURL=inferenceService.d.ts.map
